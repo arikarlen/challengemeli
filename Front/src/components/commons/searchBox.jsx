da7f4064b83bec449d434f70b1ea3c91cabe4868
Ariel Karlen <ariel.karlen@possumus.tech>
2024-04-13 16:02:10 -0300

    feature: search box

--- Front/src/components/commons/searchBox.jsx
+++ Front/src/components/commons/searchBox.jsx
@@ -1,10 +1,32 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Image, Form, InputGroup, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logoMeli.png";
+import { BsSearch } from "react-icons/bs";
 
 export function SearchBox() {
+    const handleSearch = (event) => {
+        event.preventDefault();
+        const inputValue = document.getElementById("searchInput").value;
+        window.location.href = `/items?search=${inputValue}`;
+    };
     return (
         <Container fluid id="searchBox">
             <Container>
-                <p>caja de busqueda</p>
+                <Row>
+                    <Col md={1}>
+                        <Image src={Logo} fluid />
+                    </Col>
+                    <Col md={11}>
+                        <Form>
+                            <InputGroup className="mb-3">
+                                <Form.Control id="searchInput" size="lg" type="text" placeholder="Ingrese 3 caracteres..." />
+                                <Button variant="secondary" as={Link} to="/items" onClick={handleSearch}>
+                                    <BsSearch />
+                                </Button>
+                            </InputGroup>
+                        </Form>
+                    </Col>
+                </Row>
             </Container>
         </Container>
     );
