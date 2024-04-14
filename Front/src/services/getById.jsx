a40454bcacbaa8c8bd17ae5ecbdf5fddfe2b12eb
Ariel Karlen <ariel.karlen@possumus.tech>
2024-04-14 17:44:58 -0300

    feature: connect to api

--- Front/src/services/getById.jsx
+++ Front/src/services/getById.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { BFF_API } from "../config/constants";
 
-export async function fetchItems(searchValue) {
+export async function fetchItem(id) {
     try {
-        const response = await axios.get(`${BFF_API}/items?q=${searchValue}`);
+        const response = await axios.get(`${BFF_API}/items/${id}`);
         return response.data;
     } catch (error) {
         return [];
